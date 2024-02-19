0c3788415efc182b1b969816eafa22d1d94a5c9f
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-02-19 18:56:55 -0500

    Refactor build scripts for better readability and error handling - Output executables to a new directory called output

--- run.bat
+++ run.bat
@@ -1,9 +1,55 @@
-copy src\%1.asm lib\ && ^
-cd lib && ^
-aml.exe /c /Zd /coff %1.asm && ^
-alink.exe /SUBSYSTEM:CONSOLE %1.obj && ^
-%1.exe && ^
-del %1.exe && ^
-del %1.obj && ^
-del %1.asm && ^
-cd ..
+@echo off
+
+if not exist build mkdir build
+if exist build\"%~1.exe" del build\"%~1.exe"
+
+copy src\"%~1.asm" lib\ >nul
+IF ERRORLEVEL 1 (
+    echo Failed to copy %~1.asm to lib directory.
+    goto cleanup_on_fail
+)
+
+pushd lib
+IF ERRORLEVEL 1 (
+    echo Failed to change directory to lib.
+    goto cleanup_on_fail
+)
+
+echo( && echo ----------------ASSEMBLING---------------- && echo(
+aml.exe /c /Zd /coff "%~1.asm"
+IF ERRORLEVEL 1 (
+    echo && echo Assembling %~1.asm failed.
+    popd
+    goto cleanup_on_fail
+)
+
+echo( && echo ----------------LINKING---------------- && echo(
+alink.exe /SUBSYSTEM:CONSOLE "%~1.obj" /OUT:"..\build\%~1.exe"
+IF ERRORLEVEL 1 (
+    echo && echo Linking %~1.obj failed.
+    popd
+    goto cleanup_on_fail
+)
+
+popd
+
+echo ----------------EXECUTING---------------- && echo(
+build\"%~1.exe"
+IF ERRORLEVEL 1 (
+    echo && echo Executing %~1.exe failed.
+    goto cleanup_on_fail
+)
+
+goto cleanup_on_success
+
+:cleanup_on_success
+    del lib\"%~1.asm"
+    del lib\"%~1.obj"
+    goto end
+
+:cleanup_on_fail
+    if exist lib\"%~1.asm" del lib\"%~1.asm"
+    if exist lib\"%~1.obj" del lib\"%~1.obj"
+    if exist build\"%~1.exe" del build\"%~1.exe"
+    
+:end
--- run.sh
+++ run.sh
@@ -1,10 +1,64 @@
 #!/bin/sh
-cp src/$1.asm lib 
-cd lib 
-wine aml.exe /c /Zd /coff $1.asm 
-wine alink.exe /SUBSYSTEM:CONSOLE $1.obj 
-wine $1.exe 
-rm $1.exe 
-rm $1.obj 
-rm $1.asm 
+
+[ ! -d build ] && mkdir build
+
+if [ -f build/$1.exe ]; then
+    rm build/$1.exe
+fi
+
+cp src/$1.asm lib/
+if [ $? -ne 0 ]; then
+    echo "Failed to copy $1.asm to lib directory."
+    goto_cleanup_on_fail
+fi
+
+cd lib
+if [ $? -ne 0 ]; then
+    echo "Failed to change directory to lib."
+    goto_cleanup_on_fail
+fi
+
+printf "\n----------------ASSEMBLING----------------\n\n"
+wine aml.exe /c /Zd /coff $1.asm
+if [ $? -ne 0 ]; then
+    echo "Assembly of $1.asm failed."
+    cd ..
+    goto_cleanup_on_fail
+fi
+
+printf "\n----------------LINKING----------------\n\n"
+wine alink.exe /SUBSYSTEM:CONSOLE $1.obj /OUT:"../build/$1.exe"
+if [ $? -ne 0 ]; then
+    echo "Linking $1.obj failed."
+    cd ..
+    goto_cleanup_on_fail
+fi
+
 cd ..
+
+printf "\n----------------EXECUTING----------------\n\n"
+wine build/$1.exe
+if [ $? -ne 0 ]; then
+    echo "Execution of $1.exe failed."
+    goto_cleanup_on_fail
+fi
+
+goto_cleanup_on_success
+
+goto_cleanup_on_fail() {
+    if [ -f lib/$1.asm ]; then
+        rm lib/$1.asm
+    fi
+    if [ -f lib/$1.obj ]; then
+        rm lib/$1.obj
+    fi
+    if [ -f build/$1.exe ]; then
+        rm build/$1.exe
+    fi
+    exit 1
+}
+
+goto_cleanup_on_success() {
+    rm lib/$1.asm lib/$1.obj
+    exit 0
+}
